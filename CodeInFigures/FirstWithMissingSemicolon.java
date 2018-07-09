a11f60437ab0cc10048288968c1a660077aa4435
Vaibhav <vaibhav1andonlyone@yahoo.in>
2018-07-09 16:36:53 +0800

    Fixed the errors in debugging.

--- CodeInFigures/FirstWithMissingSemicolon.java
+++ CodeInFigures/FirstWithMissingSemicolon.java
@@ -1,7 +1,6 @@
 public class FirstWithMissingSemicolon
 {
-   public static void main(String[] args)
-   {
-      System.out.println("First Java application")
+   public static void main(String[] args) {
+      System.out.println("First Java application");
    }
 }
